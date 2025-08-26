bb00c76b81dbffd1f791808eaa3d2ea54ed6a970
ZeeshanKhan-07 <khanzeeshan4876@gmail.com>
2025-08-26 08:07:39 +0530

    Changes Done

--- backend/SearchSameComments/src/main/java/com/web/SearchSameComments/repository/LoginRepo.java
+++
@@ -1,9 +0,0 @@
-package com.web.SearchSameComments.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.web.SearchSameComments.entities.Login;
-
-public interface LoginRepo  extends JpaRepository<Login, Integer>{
-    
-}
